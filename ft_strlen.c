5fe61a8c744589613e47c6636eca964c652ee346
mhnatovs <mariana.hnatovska@gmail.com>
2025-10-03 13:02:56 +0200

    Update ft_strlen.c

--- ft_strlen.c
+++ ft_strlen.c
@@ -1,14 +1,3 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_strlen.c                                        :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: mhnatovs <mhnatovs@student.42heilbronn.    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/05/18 07:56:13 by mhnatovs          #+#    #+#             */
-/*   Updated: 2025/05/18 08:04:13 by mhnatovs         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
 
 int	ft_strlen(char *str)
 {
@@ -21,13 +10,3 @@ int	ft_strlen(char *str)
 	}
 	return (i);
 }
-
-// #include <stdio.h>
-// #include <string.h>
-// int main(void)
-// {
-// 	char my_str[] = "qwerhdbjdcq-134-4jvsty";
-// 	printf("%d\n",ft_strlen(my_str));
-// 	printf("%lu",strlen(my_str));
-// 	return (0);
-// }
